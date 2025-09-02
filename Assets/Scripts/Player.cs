e34d61fcb489269629a5170cf5cb919f00caa323
Ties Spit <tiesspit@gmail.com>
2025-09-02 17:37:46 +0200

    It actually works now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,36 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
+    public float xInput;
+    public Rigidbody2D rb;
 
-    private Rigidbody2D rb;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        xInput = Input.GetAxisRaw("Horizontal");
 
-    // Update is called once per frame
-    void Update()
-    {
-        float moveInput = Input.GetAxis("horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
+        rb.linearVelocity = new Vector2(xInput, rb.linearVelocity.y);
     }
+    //public float moveSpeed = 5f;
+    //public float jumpForce = 10f;
+
+    //public Rigidbody2D rb;
+
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    float moveInput = Input.GetAxis("horizontal");
+    //    rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    //    }
+    //}
 }
